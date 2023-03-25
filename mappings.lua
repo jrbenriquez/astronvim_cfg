66315446e985d69900ceb289330aed5c059e1d76
johnreienriquez <johnrei.enriquez@gmail.com>
2023-03-25 17:25:54 +0800

    update mappings :) yay

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,9 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    --
+
+    ["<leader>s"] = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Replace in file" }, -- from thePrimeagen
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
@@ -15,14 +18,25 @@ return {
       end,
       desc = "Pick to close",
     },
+    ["tl"] ={ function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+    ["th"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer"},
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    --
+    ["<leader>fz"] = { function() require("telescope.builtin").tags() end, desc = "Find tags" },
+    ["nl"] = { "i<CR><ESC>", desc = "Break current line where the cursor is"},
+    ["nL"] = { "i<CR><ESC>O", desc = "Break current line where the cursor is and insert newline"},
+    ["gd"] = { "<C-]>", desc = "Go to Tag definition"},
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  v = {
+    ["J"] = {":m '>+1<CR>gv=gv", desc = "Move highlighted down"},
+    ["K"] = {":m '<-2<CR>gv=gv", desc = "Move highlighted up"}
+  }
 }
